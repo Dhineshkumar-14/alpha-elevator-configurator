09cadd32f4b6277bfc77bd0b5cc3753975a0701d
Dhineshkumar <dhineshkumar.velayudham@gmail.com>
2026-09-16 14:13:19 +0530

    added cookies in every req

--- backend/src/controllers/auth.controller.ts
+++ backend/src/controllers/auth.controller.ts
@@ -80,8 +80,8 @@ export const login = async (req: Request, res: Response): Promise<Response> => {
 
     res.cookie("token", token, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-      sameSite: "lax",
+      secure: true,
+      sameSite: "none",
       maxAge: 24 * 60 * 60 * 1000,
     });
 
